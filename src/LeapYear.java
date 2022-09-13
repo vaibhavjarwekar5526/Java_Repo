943df824d5e053ffb83b34112c3ab42e86f6e7b1
Vaibhav Bhaskar Jarwekar <Vaibhav.Jarwekar@ivycomptech.com>
2022-09-13 16:56:14 +0530

    added LeapYear Program file

---
+++ src/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year;
+        Scanner scanner = new Scanner(System.in);
+        year = scanner.nextInt();
+
+        if((year % 4 == 0) && ( year % 100 != 0) || (year % 400 == 0)){
+            System.out.println(year + " is leap year");
+        }
+        else{
+            System.out.println(year + " is not a leap Year");
+        }
+    }
+}
