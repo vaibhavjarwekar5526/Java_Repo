7e365dbfcd91b30ad4689b4c489f8add44c938e6
Vaibhav Bhaskar Jarwekar <Vaibhav.Jarwekar@ivycomptech.com>
2022-09-13 23:41:13 +0530

    Added few Changes

--- src/GreatestOfThreeNumbers.java
+++ src/GreatestOfThreeNumbers.java
@@ -4,6 +4,8 @@ public class GreatestOfThreeNumbers {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        System.out.println("Enter any three numbers:");
+
         int num1 = s.nextInt();
         int num2 = s.nextInt();
         int num3 = s.nextInt();
